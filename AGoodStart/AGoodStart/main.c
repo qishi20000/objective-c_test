7fc4fad030437463ccf77ef98a10de40675d75a8
qishi20000 <qishi20000@gmail.com>
2022-03-05 22:30:06 +0800

    第一个程序

---
+++ AGoodStart/AGoodStart/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by qishi20000 on 2022/3/5.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
