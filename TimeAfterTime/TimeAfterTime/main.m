bc976cb6a7ee8564258c3bbe5bb82027fafdd6f2
qishi20000 <qishi20000@gmail.com>
2022-03-09 22:55:09 +0800

    更新代码

--- TimeAfterTime/TimeAfterTime/main.m
+++ TimeAfterTime/TimeAfterTime/main.m
@@ -15,6 +15,16 @@ int main(int argc, const char * argv[]) {
         double seconds = [now timeIntervalSince1970];
         NSLog(@"It has been %f seconds since the start of 1979.",seconds);
         
+        NSDate *later = [now dateByAddingTimeInterval:1000000];
+        NSLog(@"In 100000 seconds it will be %@.",later);
+        
+        NSCalendar* cal = [NSCalendar currentCalendar];
+        NSLog(@"My calendar is %@",[cal calendarIdentifier]);
+
+        unsigned long day = [cal ordinalityOfUnit:NSCalendarUnitDay inUnit:NSCalendarUnitMonth forDate:now];
+        
+        NSLog(@"This is day %lu of the mouth",day);
+        
         NSHost* host = [NSHost currentHost];
         NSString* computer = [host localizedName];
         NSLog(@"My Computer Name: %@",computer);
